e78816f9c4da3a24bce0a341bf224f5a214ffd31
Vedblade <18503283+yad439@users.noreply.github.com>
2021-06-22 18:22:25 +0700

    Add GasStation class

---
+++ MapViewer/GasStation.cs
@@ -0,0 +1,8 @@
+using MapControl;
+
+namespace MapViewer {
+	internal sealed class GasStation {
+		public Location Location { get; }
+		public GasStation(Location location) { Location = location; }
+	}
+}
