1f2a8fd72aa74f9ea7906310093cef1dd4e0a585
Vedblade <18503283+yad439@users.noreply.github.com>
2021-06-22 16:58:09 +0700

    Make app use di

--- MapViewer/App.xaml.cs
+++ MapViewer/App.xaml.cs
@@ -1,3 +1,18 @@
-﻿namespace MapViewer {
-	internal sealed partial class App { }
+﻿using System.Diagnostics;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MapViewer {
+	internal sealed partial class App {
+		protected override void OnStartup(StartupEventArgs e) {
+			base.OnStartup(e);
+
+			var services = new ServiceCollection()
+						   .AddSingleton<MainWindow>().BuildServiceProvider();
+			var mainWindow = services.GetService<MainWindow>();
+			Debug.Assert(mainWindow != null, nameof(mainWindow) + " != null");
+			mainWindow.Show();
+			mainWindow.Loaded += (_, _) => MainWindow = mainWindow;
+		}
+	}
 }
