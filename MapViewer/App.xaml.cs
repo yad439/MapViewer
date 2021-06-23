612da827c41a7a36acec60516cc1659eced15838
Vedblade <18503283+yad439@users.noreply.github.com>
2021-06-23 17:14:24 +0700

    Add automapper

--- MapViewer/App.xaml.cs
+++ MapViewer/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Windows;
+using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MapViewer {
@@ -7,15 +8,25 @@ namespace MapViewer {
 		protected override void OnStartup(StartupEventArgs e) {
 			base.OnStartup(e);
 
+			var mapping = ConfigureMapping();
 			var services = new ServiceCollection()
 						   .AddSingleton<MainWindow>()
 						   .AddSingleton<MapViewModel>()
+						   .AddSingleton<IMapper>(_ => new Mapper(mapping))
 						   .BuildServiceProvider();
 
 			var mainWindow = services.GetService<MainWindow>();
 			Debug.Assert(mainWindow != null, nameof(mainWindow) + " != null");
 			mainWindow.Show();
 			mainWindow.Loaded += (_, _) => MainWindow = mainWindow;
+		}
+
+		private static MapperConfiguration ConfigureMapping() {
+			var mapperConfiguration = new MapperConfiguration(cfg => {
+				cfg.CreateMap<GasStation, GasStationOsmrDto>().ReverseMap();
+			});
+			mapperConfiguration.AssertConfigurationIsValid();
+			return mapperConfiguration;
 		}
 	}
 }
