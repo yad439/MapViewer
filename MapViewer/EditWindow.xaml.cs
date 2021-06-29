30654cda051c22250cccd4bd53a4b57bfa69aeb1
Vedblade <18503283+yad439@users.noreply.github.com>
2021-06-29 19:51:32 +0700

    Add editing window

---
+++ MapViewer/EditWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MapViewer {
+	internal sealed partial class EditWindow {
+		internal GasStationEditDto Data { get; }
+
+		internal EditWindow(GasStationEditDto data) {
+			Data = data;
+			InitializeComponent();
+			DataContext = data;
+			WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
+		}
+
+		private void DoneEditing(object sender, System.Windows.RoutedEventArgs e) => DialogResult = true;
+	}
+}
--- MapViewer/MainWindow.xaml.cs
+++ MapViewer/MainWindow.xaml.cs
@@ -1,11 +1,27 @@
-﻿using MapControl;
+﻿using AutoMapper;
+using MapControl;
 
 namespace MapViewer {
 	internal sealed partial class MainWindow {
-		public MainWindow(MapViewModel model) {
-			InitializeComponent();
+		private readonly IMapper _mapper;
+
+		public MainWindow(MapViewModel model, IMapper mapper) {
+			_mapper = mapper;
 			TileImageLoader.Cache = null;
+			InitializeComponent();
 			DataContext = model;
+		}
+
+		private void EditStation(object sender, System.Windows.RoutedEventArgs e) {
+			if (GasStationListBox.SelectedItem is not GasStationViewModel station) return;
+
+			var editDialog = new EditWindow(_mapper.Map<GasStationEditDto>(station)) {
+								 Owner = this
+							 };
+			var result = editDialog.ShowDialog();
+			if (result.HasValue && result.Value) {
+				_ = _mapper.Map(editDialog.Data, station);
+			}
 		}
 	}
 }
